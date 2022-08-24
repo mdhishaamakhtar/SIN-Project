0b022e2e780bf843bf60c058f64ca9a6c83ea0bd
Md Hishaam Akhtar <hishaamakhtar2001.mha@gmail.com>
2022-08-25 01:21:42 +0530

    feat: fix issue

--- routes/classifiers.py
+++ routes/classifiers.py
@@ -8,23 +8,21 @@ import json
 with open("routes/product_reviews.json") as f:
     reviews = f.read().strip().split("\n")
 reviews = [json.loads(review) for review in reviews]
-texts = [review['reviewText'] for review in reviews]
+texts = [review["reviewText"] for review in reviews]
 stop_words = set(stopwords.words("english"))
 vectorizer = TfidfVectorizer(ngram_range=(1, 2))
 vectorizer.fit_transform(texts)
 router = APIRouter(tags=["classifiers"])
 with open("routes/DT_Classifier.pkl", "rb") as fid:
     dt_classifier = pickle.load(fid)
-with open("routes/DT_Classifier.pkl", "rb") as fid:
+with open("routes/SVM_Classifier.pkl", "rb") as fid:
     svm_classifier = pickle.load(fid)
 
 
-@router.post("/create", status_code=status.HTTP_200_OK)
+@router.post("/classify", status_code=status.HTTP_200_OK)
 def create_post(review_texts: ReviewRequest):
     content = review_texts.review
-    print(content)
     words = content.split()
-    print(words)
     tokenized_text = ""
     rcount = 0
     for r in words:
@@ -33,12 +31,10 @@ def create_post(review_texts: ReviewRequest):
             rcount = rcount + 1
     if rcount == 0:
         raise HTTPException(
-            status_code=status.HTTP_404_NOT_FOUND,
-            detail="No Review Given"
+            status_code=status.HTTP_404_NOT_FOUND, detail="No Review Given"
         )
     else:
         rev_list = [tokenized_text]
-        print(rev_list)
         vec = vectorizer.transform(rev_list)
         svm_pred = svm_classifier.predict(vec)
         dt_pred = dt_classifier.predict(vec)
